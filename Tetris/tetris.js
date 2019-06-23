dc13d5d3de365b88991cdda461637eb78b7df092
LDY <1185946887@qq.com>
2019-06-23 14:21:15 +0800

    #

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -4,43 +4,59 @@ var config = {
 	"box_height":650,//游戏框长度，单位px
 	"fall_speed":1,//下落速度
 	"move_speed":2,//左右移动s速度
-	"cube_size":20,//小方块的尺寸,单位px
+	"cubes_site_size":20,//小方块的尺寸,单位px
+	center:{
+		"x":this.box_width/2,
+		"y":this.cubes_site_size*4,
+	},
 };
 /*
-*定义一个cube类，用这个cube类来实现下落的方块
+*定义一个cubes_site类，用这个cubes_site类来实现下落的方块
 */
-//site数组保存每个小方块的坐标，center保存旋转中心点的坐标
-function Cube(site,center) {
-	this.site = site;
-	this.center = center;
+//cubes_site数组保存每个小方块的坐标，center保存初始旋转中心点的坐标
+function Cube(cubes_site) {
+	this.cubes_site = cubes_site;
+	this.cubes = [];
+	this.center = config.center;
 }
 //定义类原型
 Cube.prototype = {
 	//下落
 	fall:function(){
-		for (var i = 0; i < this.site.length; i++) {
-			this.site[i]["y"] += config["fall_speed"];
+		for (var i = 0; i < this.cubes_site.length; i++) {
+			this.cubes_site[i]["y"] += config["fall_speed"];
 		}
 	},
 	//左右移动,1:右移，-1：左移
 	move:function(direction){
-		for (var i = 0; i < this.site.length; i++) {
-			this.site[i]["x"] += config["move_speed"]*direction;
+		for (var i = 0; i < this.cubes_site.length; i++) {
+			this.cubes_site[i]["x"] += config["move_speed"]*direction;
 		}
 	},
 	//旋转，direction为1，顺时针旋转九十度，-1则逆时针旋转九十度
 	rotate:function (direction) {		
-		for (var i = 0; i < this.site.length; i++) {
+		for (var i = 0; i < this.cubes_site.length; i++) {
 			var tmp = 0;
-			tmp = this.site[i]["x"];
-			this.site[i]["x"] = this.site[i]["y"]*direction;
-			this.site[i]["y"] = -tmp*direction;
+			tmp = this.cubes_site[i]["x"];
+			this.cubes_site[i]["x"] = this.cubes_site[i]["y"]*direction;
+			this.cubes_site[i]["y"] = -tmp*direction;
 		}
 		
 	},
-	//绘制方块
-	draw:function(){
+	//生成方块
+	generate:function(){
 		
+	},
+	//在box中显示方块
+	draw:function(box){
+		for (var i = 0; i < this.cubes_site.length; i++) {
+			var div = document.createElement("div");
+			div.style.top = this.cubes_site[i]["y"];
+			div.style.left = this.cubes_site[i]["x"];
+			div.className =  "cube";
+			this.cubes.push(div);
+			box.appendChild(div);
+		}
 	},
 	//碰撞检测
 	collide:function(){
@@ -69,15 +85,47 @@ Box.prototype = {
 		div.style.margin = "auto";
 		div.style.backgroundColor = "#00CCFF";
 		document.body.appendChild(div);
-		console.log(div.getBoundingClientRect());
+		var pos = div.getBoundingClientRect();
+		this.box = div;
+		this.box_x = pos.left;
+		this.box_y = pos.top;
 	},
 	//开始游戏
 	startGame:function(){
-
+		
+		this.draw();
+		this.run();
+		
+	},
+	//运行游戏
+	run:function(){
+		//定义各种不同组合的方块
+		var cubes_site = [
+			[{"x":0,"y":0}],
+			[],
+			[],
+		];
+		//投放方块到box
+		var multi = new Cube(cubes_site[0]);
+		multi.draw(this.box);
 	},
 	//结束游戏
 	endGame:function(){
 
-	}
+	},
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
